b262cd9ba1bbb48ab925f37ef7a7870c7d6c2bd1
Hktn26 <emirhaktan26@gmail.com>
2023-11-04 15:47:49 +0300

    asdasd

--- Django -5- FBV/student_api/views.py
+++ Django -5- FBV/student_api/views.py
@@ -1,8 +1,30 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view()
 def home(request):
     return Response(
         {
             "message": "Welcome to the Home Page"
         }
     )
+
+
+# ------------------------------------------------------------
+'''
+    HTTP Request Types:
+        Get -> Public verilerdir. Listeleme/Goruntuleme islemlerinde kullanilir.
+        Post -> Private verilerdir. Kayit olusturma islemlerinde kullanilir. (ID'ye ihtiyac duymaz.)
+        * Put -> Kayit guncelleme islemlerinde kullanilir. (Veri bir butun olarak guncellenir.) (ID'ye ihtiyac duyar.)
+        * Patch -> Kayit guncelleme islemlerinde kullanilir. (Verinin sadece ilgili kismi guncellenir.) (ID'ye ihtiyac duyar.)
+        * Delete -> Kayit silme islemlerinde kullanilir. (ID'ye ihtiyac duyar.)
+
+'''
+# ------------------------------------------------------------
+# StudentSeralizers Tum Kayitlari Goruntuleme
+
+from .models import Student
+
+def student_list(request):
+    student = Student.objects.all()
