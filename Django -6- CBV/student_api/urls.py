1dc9938c5e0b5039c6c62e0232b553dd7214ca74
Hktn26 <emirhaktan26@gmail.com>
2023-11-05 19:46:15 +0300

    Third CVB

--- Django -6- CBV/student_api/urls.py
+++ Django -6- CBV/student_api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
     StudentListCreate,
+    StudentDetailUpdateDelete,
 )
 
 urlpatterns = [
     path('student_list_creat/', StudentListCreate.as_view()),
+    path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
 ]
--- Django -6- CBV/student_api/views.py
+++ Django -6- CBV/student_api/views.py
@@ -6,6 +6,14 @@ from django.shortcuts import get_object_or_404
 from .models import Student
 from .serializers import StudentSerializer
 
+
+
+
+
+# -----------------------------------------------------------------------
+# https://www.django-rest-framework.org/tutorial/3-class-based-views/
+#                           APIView
+# -----------------------------------------------------------------------
 #Listeleme:
 class StudentListCreate(APIView):
 
@@ -29,13 +37,13 @@ class StudentDetailUpdateDelete(APIView):
     #Tek kayit goruntuleme:
     def get(self, request, pk):
         student = get_object_or_404(Student, id=pk)
-        serializer = StudentSerializer(instance=Student)
+        serializer = StudentSerializer(instance=student)
         return Response(serializer.data)
     
     #Tek kayit guncelle:
     def put(self, request, pk):
         student = get_object_or_404(Student, id=pk)
-        serializer = StudentSerializer(instance=Student, data=request.data)
+        serializer = StudentSerializer(instance=student, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
@@ -47,3 +55,17 @@ class StudentDetailUpdateDelete(APIView):
         student = get_object_or_404(Student, id=pk)
         student.delete()
         return Response({"Message": "Deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+# Odev Api Viewlari birlestir.
+
+
+
+
+# ------------------------------------------------------------------------------
+# https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
+#                           GenericAPIView
+# ------------------------------------------------------------------------------
+
+from rest_framework.generics import GenericAPIView
+
+# 02.02.02
