1404f8699d09ec36fd3f34385406d5e93717876b
Hktn26 <emirhaktan26@gmail.com>
2023-11-06 18:22:53 +0300

    5. CBV

--- Django -6- CBV/student_api/urls.py
+++ Django -6- CBV/student_api/urls.py
@@ -3,31 +3,32 @@ from django.urls import path
 from .views import (
     StudentListCreate,
     StudentDetailUpdateDelete,
+    StudentGPPD,
     StudentGenericListCreate,
     StudentListCreateAPIView,
-    StudentRetrieveUpdateDestroyAPIView,
-    StudentViewSet,
+    StudentRetrieveUpdateDestroyAPIView
 )
 
 urlpatterns = [
-    path('student_list_creat/', StudentListCreate.as_view()),
+    path('student_list_create/', StudentListCreate.as_view()),
     path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
-    path('StudentGenericListCreate/', StudentGenericListCreate.as_view()),
-    path('StudentListCreateAPIView/', StudentListCreateAPIView.as_view()),
-    path('StudentRetrieveUpdateDestroyAPIView/<int:pk>', StudentRetrieveUpdateDestroyAPIView.as_view()),
-    path('StudentViewSet/<int:pk>', StudentViewSet.as_view()),
+    path('student_gppd/', StudentGPPD.as_view()),
+    path('student_gppd/<int:pk>', StudentGPPD.as_view()),
+    path('student_generic_list_create/', StudentGenericListCreate.as_view()),
+    path('student_list_create_api/', StudentListCreateAPIView.as_view()),
+    path('student_get_put_delete_api/<int:pk>', StudentRetrieveUpdateDestroyAPIView.as_view()),
 ]
 
 
-
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------
 # Router for ModelViewSet
-
 from .views import StudentMVS
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('students', StudentMVS)  # URL sonunda/ yok.
+router.register('students', StudentMVS) # URL sonunda / yok.
+# router.register('another', AnotherMVS)
+# router.register('another', AnotherMVS)
+# router.register('another', AnotherMVS)
 # Add to paths:
 urlpatterns += router.urls
--- Django -6- CBV/student_api/views.py
+++ Django -6- CBV/student_api/views.py
@@ -1,4 +1,3 @@
-from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,38 +6,38 @@ from django.shortcuts import get_object_or_404
 from .models import Student
 from .serializers import StudentSerializer
 
-
-# -----------------------------------------------------------------------
+# ----------------------------------------------------------------
+# APIView
 # https://www.django-rest-framework.org/tutorial/3-class-based-views/
-#                           APIView
-# -----------------------------------------------------------------------
-# Listeleme:
+# ----------------------------------------------------------------
+
+# Listele:
 class StudentListCreate(APIView):
 
-    # Listele (GET Method)
+    # Listele (GET Method):
     def get(self, request):
         students = Student.objects.all()
         serializer = StudentSerializer(instance=students, many=True)
         return Response(serializer.data)
-
-    # Yeni kayit (POST Method)
+    
+    # Yeni Kayıt (POST Method)
     def post(self, request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class StudentDetailUpdateDelete(APIView):
-    # Tek kayit goruntuleme:
+
+    # Tek kayıt görüntüle:
     def get(self, request, pk):
         student = get_object_or_404(Student, id=pk)
         serializer = StudentSerializer(instance=student)
         return Response(serializer.data)
-
-    # Tek kayit guncelle:
+    
+    # Tek kayıt güncelle:
     def put(self, request, pk):
         student = get_object_or_404(Student, id=pk)
         serializer = StudentSerializer(instance=student, data=request.data)
@@ -47,78 +46,107 @@ class StudentDetailUpdateDelete(APIView):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # Tek Kayit silme:
+        
+    # Tek kayıt sil:
     def delete(self, request, pk):
         student = get_object_or_404(Student, id=pk)
         student.delete()
-        return Response({"Message": "Deleted"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({ "message": "Deleted" }, status=status.HTTP_204_NO_CONTENT)
+    
+# ----------------------------------------------------------------
+# Yukarıdaki işlemleri tek class'da yazmaya çalışalım
 
-# Odev Api Viewlari birlestir.
+class StudentGPPD(APIView):
 
+    def get(self, request, pk=0):
+        if pk:
+        # Tek kayıt görüntüle:
+            student = get_object_or_404(Student, id=pk)
+            serializer = StudentSerializer(instance=student)
+            return Response(serializer.data)
+        else:
+        # Kayıtları listele:
+            students = Student.objects.all()
+            serializer = StudentSerializer(instance=students, many=True)
+            return Response(serializer.data)
+    
+    # Yeni Kayıt (POST Method)
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # Tek kayıt güncelle:
+    def put(self, request, pk):
+        student = get_object_or_404(Student, id=pk)
+        serializer = StudentSerializer(instance=student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # Tek kayıt sil:
+    def delete(self, request, pk):
+        student = get_object_or_404(Student, id=pk)
+        student.delete()
+        return Response({ "message": "Deleted" }, status=status.HTTP_204_NO_CONTENT)
 
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------
+# GenericAPIView
 # https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
-#                           GenericAPIView
+# Mixins
 # https://www.django-rest-framework.org/api-guide/generic-views/#mixins
-#                               Mixins
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
 
-from rest_framework.mixins import (
-    ListModelMixin,
-    CreateModelMixin,
-    RetrieveModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin
-)
-
-class StudentGenericListCreate(ListModelMixin, CreateModelMixin, GenericAPIView):
+class StudentGenericListCreate(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    # Listeleme
+    # Listeleme:
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    # Yeni kayit
+    # Yeni Kayıt:
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
-# ------------------------------------------------------------------------------
-#                           ListCreateAPIView
-#                           RetrieveUpdateAPIView
-#                           RetrieveDestroyAPIView
-#                           RetrieveUpdateDestroyAPIView
+# ----------------------------------------------------------------
+# ListCreateAPIView
+# RetrieveUpdateDestroyAPIView
 # https://www.django-rest-framework.org/api-guide/generic-views/#concrete-view-classes
-# ------------------------------------------------------------------------------
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveUpdateAPIView,
-    RetrieveDestroyAPIView,
-    RetrieveUpdateDestroyAPIView,
-)
+# ----------------------------------------------------------------
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-
-# Kayit listeleme ve Yeni kayit ekleme:
+# Kayıt Listeleme ve Yeni Kayıt Ekleme:
 class StudentListCreateAPIView(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-
-# Tek kayir goruntuleme/ guncelleme / silme
+# Tek kayıt görüntüle/güncelle/sil:
 class StudentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # lookup_field = "id" # Default: "pk" 
+    # lookup_field = "id" # Default: "pk"
 
-# ------------------------------------------------------------------------------
-#                           ModelViewSet:
+# ----------------------------------------------------------------
+# ModelViewSet:
 # https://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
-# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
-
-#Tum islemler:
-class StudentViewSet(ModelViewSet):
+# Tüm İşlemler:
+class StudentMVS(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    @action(methods=["GET"], detail=False)
+    def count(self, request):
+        return Response({
+            "count": Student.objects.count()
+        })
