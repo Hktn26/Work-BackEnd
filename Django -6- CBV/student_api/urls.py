09785f8f633736ed43eee5b85476cc71fa56d2ea
Hktn26 <emirhaktan26@gmail.com>
2023-11-06 14:12:48 +0300

    4th CBV

--- Django -6- CBV/student_api/urls.py
+++ Django -6- CBV/student_api/urls.py
@@ -3,9 +3,31 @@ from django.urls import path
 from .views import (
     StudentListCreate,
     StudentDetailUpdateDelete,
+    StudentGenericListCreate,
+    StudentListCreateAPIView,
+    StudentRetrieveUpdateDestroyAPIView,
+    StudentViewSet,
 )
 
 urlpatterns = [
     path('student_list_creat/', StudentListCreate.as_view()),
     path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
+    path('StudentGenericListCreate/', StudentGenericListCreate.as_view()),
+    path('StudentListCreateAPIView/', StudentListCreateAPIView.as_view()),
+    path('StudentRetrieveUpdateDestroyAPIView/<int:pk>', StudentRetrieveUpdateDestroyAPIView.as_view()),
+    path('StudentViewSet/<int:pk>', StudentViewSet.as_view()),
 ]
+
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Router for ModelViewSet
+
+from .views import StudentMVS
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('students', StudentMVS)  # URL sonunda/ yok.
+# Add to paths:
+urlpatterns += router.urls
--- Django -6- CBV/student_api/views.py
+++ Django -6- CBV/student_api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,14 +8,11 @@ from .models import Student
 from .serializers import StudentSerializer
 
 
-
-
-
 # -----------------------------------------------------------------------
 # https://www.django-rest-framework.org/tutorial/3-class-based-views/
 #                           APIView
 # -----------------------------------------------------------------------
-#Listeleme:
+# Listeleme:
 class StudentListCreate(APIView):
 
     # Listele (GET Method)
@@ -22,7 +20,7 @@ class StudentListCreate(APIView):
         students = Student.objects.all()
         serializer = StudentSerializer(instance=students, many=True)
         return Response(serializer.data)
-    
+
     # Yeni kayit (POST Method)
     def post(self, request):
         serializer = StudentSerializer(data=request.data)
@@ -31,16 +29,16 @@ class StudentListCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
 
 class StudentDetailUpdateDelete(APIView):
-    #Tek kayit goruntuleme:
+    # Tek kayit goruntuleme:
     def get(self, request, pk):
         student = get_object_or_404(Student, id=pk)
         serializer = StudentSerializer(instance=student)
         return Response(serializer.data)
-    
-    #Tek kayit guncelle:
+
+    # Tek kayit guncelle:
     def put(self, request, pk):
         student = get_object_or_404(Student, id=pk)
         serializer = StudentSerializer(instance=student, data=request.data)
@@ -49,7 +47,7 @@ class StudentDetailUpdateDelete(APIView):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
     # Tek Kayit silme:
     def delete(self, request, pk):
         student = get_object_or_404(Student, id=pk)
@@ -59,13 +57,68 @@ class StudentDetailUpdateDelete(APIView):
 # Odev Api Viewlari birlestir.
 
 
-
-
 # ------------------------------------------------------------------------------
 # https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
 #                           GenericAPIView
+# https://www.django-rest-framework.org/api-guide/generic-views/#mixins
+#                               Mixins
 # ------------------------------------------------------------------------------
 
-from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
 
-# 02.02.02
+class StudentGenericListCreate(ListModelMixin, CreateModelMixin, GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # Listeleme
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # Yeni kayit
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+# ------------------------------------------------------------------------------
+#                           ListCreateAPIView
+#                           RetrieveUpdateAPIView
+#                           RetrieveDestroyAPIView
+#                           RetrieveUpdateDestroyAPIView
+# https://www.django-rest-framework.org/api-guide/generic-views/#concrete-view-classes
+# ------------------------------------------------------------------------------
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    RetrieveDestroyAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+
+
+# Kayit listeleme ve Yeni kayit ekleme:
+class StudentListCreateAPIView(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+# Tek kayir goruntuleme/ guncelleme / silme
+class StudentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # lookup_field = "id" # Default: "pk" 
+
+# ------------------------------------------------------------------------------
+#                           ModelViewSet:
+# https://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
+# ------------------------------------------------------------------------------
+from rest_framework.viewsets import ModelViewSet
+
+
+#Tum islemler:
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
