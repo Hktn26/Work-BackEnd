2b89b0e6090cc04de89f53be1b9ab086995c31fd
Hktn26 <emirhaktan26@gmail.com>
2023-11-06 19:39:32 +0300

    ToDo second Project

--- Django -7- TaskTrackerApp/main/urls.py
+++ Django -7- TaskTrackerApp/main/urls.py
@@ -22,11 +22,16 @@ urlpatterns = [
 ]
 
 # MVS - Router
-
-from rest_framework import routers
 from todo.views import TodoView
+from rest_framework import routers
+
 
 router = routers.DefaultRouter()
-router.register(TodoView)
+router.register('todo', TodoView)
 
 urlpatterns += router.urls
+
+
+# urlpatterns += [
+#     path('', router.urls)
+# ]
